89440a99636e06ff8d4328f18279be53cd91ceac
nyeonseok <aragorn2358@naver.com>
2024-04-17 23:52:21 +0900

    first

---
+++ src/main/java/nyeonseok/Yours/YoursApplication.java
@@ -0,0 +1,13 @@
+package nyeonseok.Yours;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoursApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoursApplication.class, args);
+	}
+
+}
